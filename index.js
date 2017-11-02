4f0a91fa83399eb87b592d830c91915f034032a5
Andrew Morris <andrew.morris03@mymona.uwi.edu>
2017-11-02 04:52:54 +0000

    Lab6 Exercise 4 SOlution

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
-window.onload = function returnWord (event) { 
+// window.onload = function returnWord (event) { 
         
 
-var userClick = document.getElementById("button"); 
+// var userClick = document.getElementById("button"); 
 
 
-userClick.addEventListener("click", function(event){
+// userClick.addEventListener("click", function(event){
 
-  event.preventDefault(); 
+//   event.preventDefault(); 
   
-  var httpRequest = new XMLHttpRequest();
+//   var httpRequest = new XMLHttpRequest();
 
-  var response; 
-        httpRequest.onreadystatechange = function() {
-                console.log(httpRequest);
-        if(httpRequest.readyState==4 && httpRequest.status==200){       
-          response= httpRequest.responseText;
-          var doc = document.getElementById("result");  
-          doc.innerHTML=response; 
-        }
-          }; 
+//   var response; 
+//         httpRequest.onreadystatechange = function() {
+//                 console.log(httpRequest);
+//         if(httpRequest.readyState==4 && httpRequest.status==200){       
+//           response= httpRequest.responseText;
+//           var doc = document.getElementById("result");  
+//           doc.innerHTML=response; 
+//         }
+//           }; 
    
-  var val = document.getElementById("searchbar").value; 
-  httpRequest.open("GET","request.php?q="+val,true);
-  httpRequest.send(); 
+//   var val = document.getElementById("searchbar").value; 
+//   httpRequest.open("GET","request.php?q="+val,true);
+//   httpRequest.send(); 
  
 
-}); 
+// }); 
  
  
  
+// }
+
+window.onload= function returnXML(event){ 
+  
+  var gtAUC=document.getElementById('getall'); 
+  
+  gtAUC.addEventListener("click", function (event){ 
+    
+    event.preventDefault(); 
+    
+    var http = new XMLHttpRequest(); 
+    
+    var url= "request.php?q=";  
+    var response;  
+    var condition= "&all=true"
+    
+    http.onreadystatechange= returnXML; 
+    
+    http.open('GET', url+condition); 
+    http.send(); 
+    
+    function returnXML(){ 
+      if(http.readyState==4  && http.status==200){
+        response = http.responseXML; 
+        var returnrez = document.getElementById('result');  
+        var definition = response.getElementsByTagName('definition'); 
+        var list = document.createElement('ol'); 
+        
+        returnrez.appendChild(list); 
+        for(var d = 0; d<definition.length; d++){ 
+          var define = document.createElement('li'); 
+          var text = document.createTextNode(definition[d].children[0].childNodes[0].nodeValue); 
+          define.appendChild(text); 
+          list.appendChild(define); 
+          
+        }
+      }else{ 
+        alert("Error, request cannot be completed."); 
+    }
+    
+    }  
+    
+    
+    
+  });
+  
+  
 }
-
--- request.php
+++ request.php
@@ -4,15 +4,49 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+// $definition = [
+//     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+//     "bar" => "A place that sells alcholic beverages",
+//     "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+//     "html" => "The standard markup language for creating web pages and web applications.",
+//     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+//     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+//     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+// ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+// print "<h3>" . strtoupper($query) . "</h3>";
+// print "<p>" . $definition[$query] . "</p>"; 
+
+$xmlData = '<?xml version="1.0" encoding="UTF-8"?> 
+<entries> 
+<definition name="definition" author="John">
+ a statement of the exact meaning of a word, especially in a dictionary.
+ </definition>
+ <definition name="bar" author="mary">
+ a place that sells alcholic beverages.
+ </definition>
+ <definition name="ajax" author="Kimberly">
+ technique which involves the use of javascript and xml.
+ </definition>  
+ <definition name="html" author="Bob"> 
+ The standard markup language for creating web pages and web applications.
+ </definition> 
+ <definition name ="css" author="Jim"> 
+ A style sheet language used for describing the presentation of a document written in a markup language.
+ </definition>
+<definition name="javascript" author="Yannick LynFatt"> 
+A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+</definition> 
+<definition name="php" author=" Trevor Munroe"> 
+A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+</definition>
+< entries>
+'; 
+
+header('Content-Type: text/xml'); 
+$xmlDataGo= new SimpleXMLElement(xmlData); 
+echo $trueXml = asXML();  
+
+print "<h2>" . strtoupper($query) . "</h2>"; 
+print "<p>" . $xmlData[$query] . "</p>"; 
+print "<p> -" .$xmlData[$query]. "</p>";
